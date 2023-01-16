495ba8c982d2b4ffc8c236406b2cc1fd85869105
Devin Michand <dmichand@gmail.com>
2023-01-16 13:57:13 -0600

    1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+Write a method called checkNumber with an int parameter number.
+
+        The method should not return any value, and it needs to print out:
+
+        "positive" if the parameter number is > 0
+
+        "negative" if the parameter number is < 0
+
+        "zero" if the parameter number is equal to 0
+*/
+public class Main {
+    public static void main(String[] args) {
+        checkNumber.checkNumber(10);
+    }
+}
---
+++ src/checkNumber.java
@@ -0,0 +1,11 @@
+
+public class checkNumber {
+    public static void checkNumber(int number){
+        if(number == 0)
+            System.out.println("zero");
+        else if(number > 0)
+            System.out.println("positive");
+        else
+            System.out.println("negative");
+    }
+}
